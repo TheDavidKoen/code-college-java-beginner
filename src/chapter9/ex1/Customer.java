025725c59d75561871b2e0c7c4ca77c8ed7dc880
David Koen <davidodawie@gmail.com>
2024-10-04 14:56:47 +0200

    published chapter 9

---
+++ src/chapter9/ex1/Customer.java
@@ -0,0 +1,19 @@
+package chapter9.ex1;
+
+public class Customer {
+    private String name;
+    private String ssn;
+
+    // Encapsulate this class.  Make ssn read only.
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        name = n;
+    }
+
+    public String getSSN(){
+        return ssn;
+    }
+};
