9aef91ccddcdf0081d2b9be39f18d3c599946395
David Koen <davidodawie@gmail.com>
2024-10-09 17:16:52 +0200

    published chapter 14

---
+++ src/chapter14/ex1/ShoppingCart.java
@@ -0,0 +1,17 @@
+package chapter14.ex1;
+
+public class ShoppingCart {
+    public static void main(String[] args){
+        Calculator calc = new Calculator();
+        int addResult = calc.add(43, 79);
+        System.out.println("Add Result: "+addResult);
+
+        // Handle possible ArithmeticException
+        try {
+            double divResult = calc.divide(15, 0);
+            System.out.println("Division Result: "+divResult);
+        }catch (ArithmeticException ae){
+            System.out.println("Hi Gwen");
+        }
+    }
+};
